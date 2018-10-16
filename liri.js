e54f3dba00ec31519f3a9b91088e62175ead603c
Kona Tess <konatess@gmail.com>
2018-10-15 21:51:48 -0700

    Got the keys working for spotify.

--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var keys = require('keys.js');
+var Spotify = require('node-spotify-api');
+
+var fs = require('fs');
+
+var keys = require('./keys.js');
 
 var spotify = new Spotify(keys.spotify);
 
-console.log(spotify)
+var commands = ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says']
